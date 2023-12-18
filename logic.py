5883defe9c9e929a189f94f01fd75b3a349d7715
hadjm <hadjmahmoudbederina@gmail.com>
2023-12-18 05:17:08 +0100

    Refactored main and logic modules for OOP

--- logic.py
+++ logic.py
@@ -5,96 +5,106 @@ from multiprocessing import Process, Value
 import numpy as np
 
 
-def init_pygame():
-    """
-    Initialize pygame for video display.
-    """
-    pygame.init()
+class Monitor:
+    def __init__(self, monitor_number):
+        self.monitor_number = monitor_number
+        self.screen = None
+        self.setup_monitor()
 
+    def setup_monitor(self):
+        monitors = get_monitors()
+        if self.monitor_number >= len(monitors):
+            raise ValueError(f"Monitor {self.monitor_number} not found. Available monitors: {len(monitors)}")
 
-def fade_in_effect(screen, final_frame, duration=2):
-    """
-    Apply fade-in effect to the screen.
-    """
-    fade_in_steps = 30
-    for alpha in np.linspace(0, 255, fade_in_steps):
-        fade_frame = cv2.addWeighted(final_frame, alpha / 255, np.zeros_like(final_frame), 1 - alpha / 255, 0)
-        frame_surface = pygame.surfarray.make_surface(fade_frame)
-        screen.blit(pygame.transform.rotate(pygame.transform.flip(frame_surface, True, False), 90), (0, 0))
-        pygame.display.update()
-        pygame.time.delay(int((duration / fade_in_steps) * 1000))  # Fade-in step duration
+        monitor = monitors[self.monitor_number]
+        self.screen = pygame.display.set_mode((monitor.width, monitor.height), pygame.FULLSCREEN,
+                                              display=self.monitor_number)
 
+    def play_video(self, video_path, should_run):
+        cap = cv2.VideoCapture(video_path)
 
-def play_video_on_monitor(video_path, monitor_number, should_run):
-    """
-    Plays a video on a specified monitor with a fade-in effect at the start.
-    """
-    monitors = get_monitors()
-    if monitor_number >= len(monitors):
-        print(f"Monitor {monitor_number} not found. Available monitors: {len(monitors)}")
-        return
+        if not cap.isOpened():
+            raise IOError(f"Cannot open video file {video_path}")
 
-    monitor = monitors[monitor_number]
-    screen = pygame.display.set_mode((monitor.width, monitor.height), pygame.FULLSCREEN, display=monitor_number)
-    cap = cv2.VideoCapture(video_path)
-
-    if not cap.isOpened():
-        print(f"Cannot open video file {video_path}")
-        return
-
-    ret, first_frame = cap.read()
-    if not ret:
-        print(f"Failed to read the first frame from {video_path}")
-        return
-
-    # Convert first frame color and apply fade-in effect
-    first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2RGB)
-    fade_in_effect(screen, first_frame)
-
-    while should_run.value:
-        ret, frame = cap.read()
+        ret, first_frame = cap.read()
         if not ret:
-            cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Restart the video
-            continue
+            raise IOError(f"Failed to read the first frame from {video_path}")
 
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame_surface = pygame.surfarray.make_surface(frame)
-        screen.blit(pygame.transform.rotate(pygame.transform.flip(frame_surface, True, False), 90), (0, 0))
-        pygame.display.update()
+        first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2RGB)
+        self.fade_in_effect(first_frame)
 
-    cap.release()
-    pygame.quit()
+        while should_run.value:
+            ret, frame = cap.read()
+            if not ret:
+                cap.set(cv2.CAP_PROP_POS_FRAMES, 0)  # Restart the video
+                continue
 
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            frame_surface = pygame.surfarray.make_surface(frame)
+            self.screen.blit(pygame.transform.rotate(pygame.transform.flip(frame_surface, True, False), 90), (0, 0))
+            pygame.display.update()
 
-def start_video_sequence(videos, should_run):
-    """
-    Starts a new video sequence on multiple monitors.
-    """
-    processes = []
+        cap.release()
 
-    for monitor_number, video_path in enumerate(videos):
-        should_run.value = True
-        p = Process(target=play_video_on_monitor, args=(video_path, monitor_number, should_run))
-        p.start()
-        processes.append(p)
-
-    return processes
-
-
-def stop_video_sequence(processes, should_run):
-    """
-    Stops the current video sequence.
-    """
-    should_run.value = False
-    for p in processes:
-        p.join()
+    def fade_in_effect(self, final_frame, duration=2):
+        fade_in_steps = 30
+        for alpha in np.linspace(0, 255, fade_in_steps):
+            fade_frame = cv2.addWeighted(final_frame, alpha / 255, np.zeros_like(final_frame), 1 - alpha / 255, 0)
+            frame_surface = pygame.surfarray.make_surface(fade_frame)
+            self.screen.blit(pygame.transform.rotate(pygame.transform.flip(frame_surface, True, False), 90), (0, 0))
+            pygame.display.update()
+            pygame.time.delay(int((duration / fade_in_steps) * 1000))
 
 
-def display_black_screen():
-    """
-    Displays a black screen on all monitors.
-    """
-    for monitor in get_monitors():
-        pygame.display.set_mode((monitor.width, monitor.height), pygame.FULLSCREEN, display=monitor.number)
-        pygame.display.get_surface().fill((0, 0, 0))
-        pygame.display.update()
+class VideoSequence:
+    def __init__(self, sequences):
+        self.sequences = sequences
+        self.should_run = Value('i', False)
+        self.processes = []
+
+    def start_sequence(self, sequence_key):
+        if sequence_key not in self.sequences:
+            raise ValueError(f"Sequence {sequence_key} not found.")
+
+        videos = self.sequences[sequence_key]
+        self.stop_sequence()  # Stop any currently running sequence
+        self.should_run.value = True
+
+        for monitor_number, video_path in enumerate(videos):
+            p = Process(target=self.play_video_on_monitor, args=(video_path, monitor_number, self.should_run))
+            p.start()
+            self.processes.append(p)
+
+    def stop_sequence(self):
+        self.should_run.value = False
+        for p in self.processes:
+            p.join()
+        self.processes = []
+
+    @staticmethod
+    def play_video_on_monitor(video_path, monitor_number, should_run):
+        monitor = Monitor(monitor_number)
+        monitor.play_video(video_path, should_run)
+
+
+class Application:
+    def __init__(self, sequences):
+        self.video_sequence = VideoSequence(sequences)
+        pygame.init()
+
+    def run(self):
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.unicode in self.video_sequence.sequences:
+                        self.video_sequence.start_sequence(event.unicode)
+                    elif event.unicode == '0':
+                        self.video_sequence.stop_sequence()
+                        self.display_black_screen()
+
+    @staticmethod
+    def display_black_screen():
+        for monitor in get_monitors():
+            pygame.display.set_mode((monitor.width, monitor.height), pygame.FULLSCREEN, display=monitor.number)
+            pygame.display.get_surface().fill((0, 0, 0))
+            pygame.display.update()
--- main.py
+++ main.py
@@ -10,21 +10,8 @@ def main():
         '2': ["path_to_video_sequence2_monitor1.mp4", "path_to_video_sequence2_monitor2.mp4"],
         # Add more sequences as needed
     }
-
-    should_run = Value('i', False)
-    current_processes = []
-    init_pygame()
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.KEYDOWN:
-                if event.unicode in sequences:
-                    stop_video_sequence(current_processes, should_run)
-                    current_processes = start_video_sequence(sequences[event.unicode], should_run)
-                elif event.unicode == '0':
-                    stop_video_sequence(current_processes, should_run)
-                    current_processes = []
-                    display_black_screen()
+    app = Application(sequences)
+    app.run()
 
 
 if __name__ == "__main__":
