ed15cf2825024d1c55942688809b50b744579765
hadjm <hadjmahmoudbederina@gmail.com>
2023-12-18 05:32:43 +0100

    Modified main function to launch the GUI

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
-from logic import *
+from gui import VideoSequenceConfigurator
+import tkinter as tk
+from logic import Application
 
 
-def main():
+def main(gui=True):
     """
     Main function to control video playback based on user input.
     """
+    if gui:
+        root = tk.Tk()
+        app = VideoSequenceConfigurator(root)
+        app.load_configuration()
+        root.mainloop()
+
+        # After closing the GUI, run the application with the saved configuration
+        if app.config:  # Check if configuration exists
+            application = Application(app.config)
+            application.run()
+
+        return
     sequences = {
         '1': ["path_to_video_sequence1_monitor1.mp4", "path_to_video_sequence1_monitor2.mp4"],
         '2': ["path_to_video_sequence2_monitor1.mp4", "path_to_video_sequence2_monitor2.mp4"],
