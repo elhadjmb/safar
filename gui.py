60623bd12fd549471e3b7367662a15dcd4c3e50e
hadjm <hadjmahmoudbederina@gmail.com>
2023-12-18 05:46:23 +0100

    Improved the UI layout

--- gui.py
+++ gui.py
@@ -10,16 +10,15 @@ class VideoSequenceConfigurator:
     def __init__(self, root):
         self.root = root
         self.root.title("Video Sequence Configurator")
-        self.root.geometry("500x400")  # Set initial size of the window
+        self.root.geometry("600x500")
 
         self.config = {"sequences": {}, "hotkeys": {}}
         self.setup_ui()
 
     def setup_ui(self):
         self.main_frame = ttk.Frame(self.root)
-        self.main_frame.pack(fill="both", expand=True, padx=10, pady=10)
+        self.main_frame.pack(fill="both", expand=True, padx=20, pady=20)
 
-        # Scrollable frame to hold sequences
         self.canvas = tk.Canvas(self.main_frame)
         self.canvas.pack(side="left", fill="both", expand=True)
 
@@ -35,7 +34,8 @@ class VideoSequenceConfigurator:
         self.add_sequence_button = ttk.Button(self.root, text="Add Video Sequence", command=self.add_video_sequence)
         self.add_sequence_button.pack(pady=10)
 
-        self.save_config_button = ttk.Button(self.root, text="Save Configuration", command=self.save_configuration)
+        self.save_config_button = ttk.Button(self.root, text="Save Configuration",
+                                             command=self.confirm_save_configuration)
         self.save_config_button.pack(pady=10)
 
     def add_video_sequence(self):
@@ -43,33 +43,50 @@ class VideoSequenceConfigurator:
         sequence_key = str(sequence_id)
 
         sequence_frame = ttk.LabelFrame(self.sequence_frame, text=f"Sequence {sequence_id}")
-        sequence_frame.pack(padx=10, pady=5, fill="x", expand=True)
+        sequence_frame.pack(padx=10, pady=10, fill="x", expand=True)
 
         monitors = self.get_monitors()
         videos = []
         for monitor_num in range(len(monitors)):
-            file_button = ttk.Button(sequence_frame, text=f"Select Video for Monitor {monitor_num + 1}",
-                                     command=lambda m=monitor_num: self.select_video_file(sequence_key, m))
-            file_button.pack(padx=5, pady=5)
-            videos.append("")
+            monitor_frame = ttk.Frame(sequence_frame)
+            monitor_frame.pack(padx=10, pady=5, fill="x", expand=True)
 
-        hotkey_label = ttk.Label(sequence_frame, text="Hotkey:")
-        hotkey_label.pack(side="left", padx=5)
-        hotkey_entry = ttk.Entry(sequence_frame)
-        hotkey_entry.pack(side="left", padx=5)
-        hotkey_entry.bind("<Return>", lambda event, sk=sequence_key: self.set_hotkey(sk, hotkey_entry.get()))
+            file_button = ttk.Button(monitor_frame, text=f"Select Video for Monitor {monitor_num + 1}",
+                                     command=lambda m=monitor_num: self.select_video_file(sequence_key, m))
+            file_button.pack(side="left", padx=10)
+
+            file_label = ttk.Label(monitor_frame, text="No file selected", width=40)
+            file_label.pack(side="left", padx=10)
+            videos.append({"button": file_button, "label": file_label})
 
         self.config["sequences"][sequence_key] = videos
+
+        hotkey_frame = ttk.Frame(sequence_frame)
+        hotkey_frame.pack(padx=10, pady=5, fill="x", expand=True)
+
+        hotkey_label = ttk.Label(hotkey_frame, text="Hotkey:")
+        hotkey_label.pack(side="left", padx=5)
+        hotkey_entry = ttk.Entry(hotkey_frame)
+        hotkey_entry.pack(side="left", padx=5)
+        hotkey_entry.bind("<Return>", lambda event, sk=sequence_key: self.set_hotkey(sk, hotkey_entry.get()))
 
     def select_video_file(self, sequence_key, monitor_num):
         filepath = filedialog.askopenfilename(title="Select Video File", filetypes=[("Video Files", "*.mp4;*.avi")])
         if filepath:
+            self.config["sequences"][sequence_key][monitor_num]["label"].configure(text=os.path.basename(filepath))
+            self.config["sequences"][sequence_key][monitor_num]["button"].configure(text="Change Video")
             self.config["sequences"][sequence_key][monitor_num] = filepath
-            messagebox.showinfo("File Selected", f"Video for Monitor {monitor_num + 1} has been selected.")
 
     def set_hotkey(self, sequence_key, hotkey):
+        if hotkey in self.config["hotkeys"]:
+            messagebox.showerror("Error", f"The hotkey '{hotkey}' is already assigned.")
+            return
         self.config["hotkeys"][hotkey] = sequence_key
         messagebox.showinfo("Hotkey Set", f"Hotkey for Sequence {sequence_key} set to '{hotkey}'.")
+
+    def confirm_save_configuration(self):
+        if messagebox.askokcancel("Confirm Save", "Are you sure you want to save this configuration?"):
+            self.save_configuration()
 
     def save_configuration(self):
         with open('config.json', 'w') as config_file:
