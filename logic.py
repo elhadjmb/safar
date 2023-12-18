005652a0d330b554c6f7b968e39e245b9b84093d
hadjm <hadjmahmoudbederina@gmail.com>
2023-12-18 05:13:57 +0100

    Added fade_in_effect function

--- logic.py
+++ logic.py
@@ -2,6 +2,7 @@ import cv2
 import pygame
 from screeninfo import get_monitors
 from multiprocessing import Process, Value
+import numpy as np
 
 
 def init_pygame():
@@ -11,9 +12,22 @@ def init_pygame():
     pygame.init()
 
 
+def fade_in_effect(screen, final_frame, duration=2):
+    """
+    Apply fade-in effect to the screen.
+    """
+    fade_in_steps = 30
+    for alpha in np.linspace(0, 255, fade_in_steps):
+        fade_frame = cv2.addWeighted(final_frame, alpha / 255, np.zeros_like(final_frame), 1 - alpha / 255, 0)
+        frame_surface = pygame.surfarray.make_surface(fade_frame)
+        screen.blit(pygame.transform.rotate(pygame.transform.flip(frame_surface, True, False), 90), (0, 0))
+        pygame.display.update()
+        pygame.time.delay(int((duration / fade_in_steps) * 1000))  # Fade-in step duration
+
+
 def play_video_on_monitor(video_path, monitor_number, should_run):
     """
-    Plays a video on a specified monitor.
+    Plays a video on a specified monitor with a fade-in effect at the start.
     """
     monitors = get_monitors()
     if monitor_number >= len(monitors):
@@ -28,6 +42,15 @@ def play_video_on_monitor(video_path, monitor_number, should_run):
         print(f"Cannot open video file {video_path}")
         return
 
+    ret, first_frame = cap.read()
+    if not ret:
+        print(f"Failed to read the first frame from {video_path}")
+        return
+
+    # Convert first frame color and apply fade-in effect
+    first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2RGB)
+    fade_in_effect(screen, first_frame)
+
     while should_run.value:
         ret, frame = cap.read()
         if not ret:
@@ -35,8 +58,8 @@ def play_video_on_monitor(video_path, monitor_number, should_run):
             continue
 
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        frame = pygame.surfarray.make_surface(frame)
-        screen.blit(pygame.transform.rotate(pygame.transform.flip(frame, True, False), 90), (0, 0))
+        frame_surface = pygame.surfarray.make_surface(frame)
+        screen.blit(pygame.transform.rotate(pygame.transform.flip(frame_surface, True, False), 90), (0, 0))
         pygame.display.update()
 
     cap.release()
