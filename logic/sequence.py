e87cd3340ab92e1fbf00d061b29e9b88175a02ea
hadjm <hadjmahmoudbederina@gmail.com>
2023-12-20 08:32:58 +0100

    Added BlackScreen class with its methods and functionalities

--- logic/sequence.py
+++ logic/sequence.py
@@ -3,6 +3,52 @@ from multiprocessing import Process
 import cv2
 
 from .monitor import Monitor
+import cv2
+import numpy as np
+
+
+class BlackScreen:
+    """
+    Simulates a video displaying a black screen.
+
+    Attributes:
+        duration (int): Duration for the black screen in seconds.
+        frame_rate (int): Frame rate of the simulated video.
+    """
+
+    def __init__(self, duration=10, frame_rate=30):
+        """
+        Initialize a BlackScreen instance.
+
+        Args:
+            duration (int): Duration for the black screen in seconds. Defaults to 10.
+            frame_rate (int): Frame rate of the simulated video. Defaults to 30.
+        """
+        self.duration = duration
+        self.frame_rate = frame_rate
+
+    def play(self, window_name, monitor):
+        """
+        Play the black screen on the specified monitor.
+
+        Args:
+            window_name (str): The name of the window for display.
+            monitor (Monitor): The monitor on which to display the black screen.
+        """
+        # Create a black frame
+        black_frame = np.zeros((monitor.height, monitor.width, 3), dtype=np.uint8)
+
+        cv2.namedWindow(window_name, cv2.WND_PROP_FULLSCREEN)
+        cv2.moveWindow(window_name, monitor.x, monitor.y)
+        cv2.setWindowProperty(window_name, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        num_frames = self.duration * self.frame_rate
+        for _ in range(num_frames):
+            cv2.imshow(window_name, black_frame)
+            if cv2.waitKey(int(1000 / self.frame_rate)) & 0xFF == ord('q'):
+                break
+
+        cv2.destroyAllWindows()
 
 
 class Sequence:
@@ -31,9 +77,20 @@ class Sequence:
         self.monitors = monitors or Monitor.get_monitors()
         self._processes = []
 
+        if not videos:
+            # If no videos are specified, map black screens to all monitors.
+            self._map_black_screens_to_monitors()
+
         # Ensure videos and monitors are mapped correctly.
         if len(self.videos) != len(self.monitors):
             raise ValueError("The number of videos must match the number of monitors.")
+
+    def _map_black_screens_to_monitors(self):
+        """
+        Maps black screens to all detected monitors.
+        """
+        # Assuming BlackScreen is a Video-like class displaying a black screen.
+        self.videos = [BlackScreen() for _ in self.monitors]
 
     def start(self):
         """
