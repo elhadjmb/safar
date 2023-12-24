20ff13a3a85bf18ffb5c3b0ea87784769f088ec7
hadjm <hadjmahmoudbederina@gmail.com>
2023-12-24 13:28:57 +0100

    Updated UI layout

--- interface/edit.py
+++ interface/edit.py
@@ -56,25 +56,35 @@ class ConfigGUI(ConfigBackend):
         self.setup_widgets()
 
     def setup_widgets(self):
-        # Main frame
+        # Main frame with improved layout
         self.main_frame = ctk.CTkFrame(self.root)
-        self.main_frame.pack(fill="both", expand=True, padx=10, pady=10)
+        self.main_frame.pack(fill="both", expand=True, padx=20, pady=20)
 
-        # Screen detection section
+        # Header Label
+        self.header_label = ctk.CTkLabel(self.main_frame, text="Screen Configuration Tool", font=("Arial", 14, "bold"))
+        self.header_label.pack(pady=(0, 10))
+
+        # Screen detection section with clear labeling
         self.detect_button = ctk.CTkButton(self.main_frame, text="Detect Screens", command=self.on_detect_screens)
-        self.detect_button.pack(pady=5)
+        self.detect_button.pack(pady=(0, 10))
 
-        # Detected screens frame
+        # Detected screens frame with label
+        self.detected_screens_label = ctk.CTkLabel(self.main_frame, text="Detected Screens:", font=("Arial", 12))
+        self.detected_screens_label.pack(pady=(5, 0))
+
         self.detected_screens_frame = ctk.CTkFrame(self.main_frame)
-        self.detected_screens_frame.pack(pady=5, fill="both", expand=True)
+        self.detected_screens_frame.pack(pady=(0, 10), fill="both", expand=True)
 
-        # Configuration screens frame
+        # Configuration screens frame with label
+        self.config_screens_label = ctk.CTkLabel(self.main_frame, text="Configuration Screens:", font=("Arial", 12))
+        self.config_screens_label.pack(pady=(5, 0))
+
         self.config_screens_frame = ctk.CTkFrame(self.main_frame)
-        self.config_screens_frame.pack(pady=5, fill="both", expand=True)
+        self.config_screens_frame.pack(pady=(0, 10), fill="both", expand=True)
 
-        # Save button
+        # Save button with improved positioning
         self.save_button = ctk.CTkButton(self.main_frame, text="Save Configuration", command=self.on_save_config)
-        self.save_button.pack(pady=5)
+        self.save_button.pack(pady=(10, 0))
 
     def on_detect_screens(self):
         self.detect_screens()
@@ -82,42 +92,39 @@ class ConfigGUI(ConfigBackend):
         self.display_config_screens()
 
     def display_detected_screens(self):
-        # Clear previous widgets
         for widget in self.detected_screens_frame.winfo_children():
             widget.destroy()
 
-        # Display detected screens with Blink button
         for i, screen in enumerate(self.detected_screens):
-            screen_info_frame = ctk.CTkFrame(self.detected_screens_frame)
-            screen_info_frame.pack(pady=2, fill="x", expand=True)
+            screen_frame = ctk.CTkFrame(self.detected_screens_frame)
+            screen_frame.pack(pady=2, fill="x", expand=True)
+            screen_frame.grid_columnconfigure(1, weight=1)  # Ensure label expands
 
-            label = ctk.CTkLabel(screen_info_frame, text=f"Detected Screen {i}: {screen.name}")
-            label.pack(side="left", padx=5)
+            label = ctk.CTkLabel(screen_frame, text=f"Detected Screen {i}: {screen.name}")
+            label.grid(row=0, column=0, sticky="w", padx=5)
 
-            blink_button = ctk.CTkButton(screen_info_frame, text="Blink", command=lambda s=screen: s.blink())
-            blink_button.pack(side="left", padx=5)
+            blink_button = ctk.CTkButton(screen_frame, text="Blink", command=lambda s=screen: s.blink())
+            blink_button.grid(row=0, column=1, sticky="e", padx=5)
 
     def display_config_screens(self):
-        # Clear previous widgets
         for widget in self.config_screens_frame.winfo_children():
             widget.destroy()
 
-        # Display screens from config
         for i, screen in enumerate(self.config.screens):
-            label = ctk.CTkLabel(self.config_screens_frame, text=f"Config Screen {i}: {screen.name}")
-            label.pack(side="left", padx=5)
+            config_frame = ctk.CTkFrame(self.config_screens_frame)
+            config_frame.pack(pady=2, fill="x", expand=True)
+            config_frame.grid_columnconfigure(1, weight=1)  # Ensure label expands
 
-            # Dropdown for remapping
-            screen_options = [f"Screen {j}" for j in range(len(self.detected_screens))]
-            screen_var = tk.StringVar(value=screen_options[0])
-            dropdown = ctk.CTkOptionMenu(self.config_screens_frame, variable=screen_var, values=screen_options)
-            dropdown.pack(side="left", padx=5)
+            label = ctk.CTkLabel(config_frame, text=f"Config Screen {i}: {screen.name}")
+            label.grid(row=0, column=0, sticky="w", padx=5)
 
-            # Remap button
-            remap_button = ctk.CTkButton(self.config_screens_frame, text="Remap",
+            screen_var = tk.StringVar(value=[f"Screen {j}" for j in range(len(self.detected_screens))][0])
+            dropdown = ctk.CTkOptionMenu(config_frame, variable=screen_var, values=screen_var.get())
+            dropdown.grid(row=0, column=1, padx=5)
+
+            remap_button = ctk.CTkButton(config_frame, text="Remap",
                                          command=lambda i=i, var=screen_var: self.remap_and_update(i, var))
-            remap_button.pack(side="left", padx=5)
-
+            remap_button.grid(row=0, column=2, padx=5)
     def remap_and_update(self, config_screen_id, screen_var):
         detected_screen_id = int(screen_var.get().split(" ")[1])
         self.remap_screen(config_screen_id, detected_screen_id)
@@ -126,3 +133,9 @@ class ConfigGUI(ConfigBackend):
     def on_save_config(self):
         self.save_config()
         messagebox.showinfo("Success", "Configuration Saved Successfully")
+
+
+# Create and run the GUI
+root = ctk.CTk()
+app = ConfigGUI(root)
+root.mainloop()
